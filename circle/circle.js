e9dcf7940f2ec4afc5b8038f1e17273199847cdb
fahdjamy <fahdjamy2@gmail.com>
2022-08-06 13:28:58 -0500

    Implement the circle lab

---
+++ circle/circle.js
@@ -0,0 +1,57 @@
+let resizeInterval = setInterval(changeCircleSize, 2500);
+
+let curSize = 0;
+let growthAmnt = 10;
+function changeCircleSize() {
+  const circles = document.getElementsByClassName("circle");
+  curSize = curSize + growthAmnt;
+  for (let crl of circles) {
+    crl.style.width = `${curSize}px`;
+    crl.style.height = `${curSize}px`;
+  }
+}
+
+function stopResizing() {
+  clearInterval(resizeInterval);
+  const circles = document.getElementsByClassName("circle");
+  for (let circle of circles) {
+    circle.style.display = "none";
+  }
+  curSize = 0;
+}
+
+function onStartBtnClicked() {
+  const noOfcircles = document.getElementById("noOfcircles").value;
+  const circleWidth = document.getElementById("circleWidth").value;
+  const growthAmount = document.getElementById("growthAmt").value;
+  const growthRate = document.getElementById("growthRate").value;
+  growthAmnt = parseInt(growthAmount);
+  
+  const msgDisplay = document.getElementById("msgDisplay");
+  if (!circleWidth || !growthRate || !growthAmount || !noOfcircles) {
+    const spanMsg = document.createElement("span");
+    spanMsg.textContent = "Please fill in all the fields.";
+    msgDisplay.style.display = "block";
+    msgDisplay.style.color = "red";
+    msgDisplay.append(spanMsg);
+    return;
+  }
+  msgDisplay.style.display = "none";
+  
+  const circles = [];
+  for (let i = 0; i < noOfcircles; i++) {
+    let circle = document.createElement("div");
+    circle.classList.add("circle");
+    circle.style.display = "block";
+    circle.style.width = `${circleWidth}px`;
+    circle.style.height = `${circleWidth}px`;
+    circle.onclick = stopResizing;
+    circles.push(circle);
+  }
+  const circleContainer = document.getElementById("circleContainer");
+
+  for (let crl of circles) {
+    circleContainer.append(crl);
+  }
+  resizeInterval = setInterval(changeCircleSize, parseInt(growthRate));
+}
